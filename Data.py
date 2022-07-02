50bf5f6d9e317bc8766775c07934a3def2fc6ba9
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-07-02 10:18:18 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -3,7 +3,7 @@ from pyrogram.types import InlineKeyboardButton
 
 class Data:
     # Start Message
-    START = """ɪғ ʏᴏᴜ ʜᴀᴠᴇ ᴀɴʏ ǫᴜᴇsᴛɪᴏɴs ᴀʙᴏᴜᴛ ᴍᴇ ᴛʜᴇɴ ᴅᴍ ᴛᴏ ᴍʏ [ᴏᴡɴᴇʀ](https://t.me/OFFICIALHACKERERA) [!](https://te.legra.ph/file/4f730af88f1d7ec343386.jpg)"""
+    START = """ [•](https://te.legra.ph/file/4f730af88f1d7ec343386.jpg)"""
 
     # Home Button
     home_buttons = [
