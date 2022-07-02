e7e2ed01f8841e8c106811974cc5c1a1409ae6ec
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-07-02 10:10:10 +0530

    Update generate.py

--- StringSessionBot/generate.py
+++ StringSessionBot/generate.py
@@ -27,8 +27,8 @@ async def main(_, msg):
     await msg.reply(
         "Please choose the python library you want to generate string session for",
         reply_markup=InlineKeyboardMarkup([[
-            InlineKeyboardButton("Pyrogram", callback_data="pyrogram"),
-            InlineKeyboardButton("Telethon", callback_data="telethon")
+            InlineKeyboardButton("• ᴘʏʀᴏɢʀᴀᴍ •", callback_data="pyrogram"),
+            InlineKeyboardButton("• ᴛᴇʟᴇᴛʜᴏɴ •", callback_data="telethon")
         ]])
     )
 
